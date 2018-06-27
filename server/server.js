ea18e2474eb829469e82d9c60ffedcf006fb49a4
Ekam Singh Chahal <ecsingh1997@gmail.com>
2018-06-27 18:16:06 +0530

    socket.io added

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');  // using path for addressing is much easier and nicer thing to do //
 var app = express();
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));   // app.use(express.static(root))  root(absolute path) here must be from __dirname
 
-const port = process.env.PORT || 3000;
+io.on('connection',(socket) => {
+  console.log('User connected');
+  socket.on('disconnect',() =>{
+    console.log('User disconnected');
+  });
+});
 
 app.get('/',(req,res) => {
   res.render('index.html');
 });
 
-app.listen(port,() => {
+server.listen(port,() => {
   console.log(`App started on Port ${port}`);
 });
